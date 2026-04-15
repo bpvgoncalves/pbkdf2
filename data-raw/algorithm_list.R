adc8a23c36d12cbe0d0e66fb8fe6cd82c2ff77c0
Bruno Gonçalves <bpvgoncalves@users.noreply.github.com>
2026-04-16 00:36:21 +0200

    refactor: change algorithm list data structure

--- data-raw/algorithm_list.R
+++ data-raw/algorithm_list.R
@@ -1,17 +1,17 @@
-df <- data.frame(oid = NA, frdly_name = NA)
+df <- data.frame(oid = NA, short_name = NA, frdly_name = NA, len = NA)
 
 # HMAC - Algorithms ----
-df <- rbind(df, list("1.3.6.1.5.5.8.1.2",       "HMAC_SHA1"))
+df <- rbind(df, list("1.3.6.1.5.5.8.1.2",       "SHA1",     "HMAC_SHA1",     20L))
 
-df <- rbind(df, list("1.2.840.113549.2.8",      "HMAC_SHA224"))
-df <- rbind(df, list("1.2.840.113549.2.9",      "HMAC_SHA256"))
-df <- rbind(df, list("1.2.840.113549.2.10",     "HMAC_SHA384"))
-df <- rbind(df, list("1.2.840.113549.2.11",     "HMAC_SHA512"))
+df <- rbind(df, list("1.2.840.113549.2.8",      "SHA224",   "HMAC_SHA224",   28L))
+df <- rbind(df, list("1.2.840.113549.2.9",      "SHA256",   "HMAC_SHA256",   32L))
+df <- rbind(df, list("1.2.840.113549.2.10",     "SHA384",   "HMAC_SHA384",   48L))
+df <- rbind(df, list("1.2.840.113549.2.11",     "SHA512",   "HMAC_SHA512",   64L))
 
-df <- rbind(df, list("2.16.840.1.101.3.4.2.13", "HMAC_SHA3_224"))
-df <- rbind(df, list("2.16.840.1.101.3.4.2.14", "HMAC_SHA3_256"))
-df <- rbind(df, list("2.16.840.1.101.3.4.2.15", "HMAC_SHA3_384"))
-df <- rbind(df, list("2.16.840.1.101.3.4.2.16", "HMAC_SHA3_512"))
+df <- rbind(df, list("2.16.840.1.101.3.4.2.13", "SHA3_224", "HMAC_SHA3_224", 28L))
+df <- rbind(df, list("2.16.840.1.101.3.4.2.14", "SHA3_256", "HMAC_SHA3_256", 32L))
+df <- rbind(df, list("2.16.840.1.101.3.4.2.15", "SHA3_384", "HMAC_SHA3_384", 48L))
+df <- rbind(df, list("2.16.840.1.101.3.4.2.16", "SHA3_512", "HMAC_SHA3_512", 64L))
 
 # Save data for internal use
 algorithm_list <- na.omit(df)
